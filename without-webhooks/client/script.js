477fa02ff8aa79fee39279e5d30883c23bdeefc6
Adrienne Dreyfus <adreyfus@stripe.com>
2019-11-04 14:34:36 -0800

    Updated to use confirmCardPayment

--- without-webhooks/client/script.js
+++ without-webhooks/client/script.js
@@ -81,8 +81,7 @@ var handleAction = function(clientSecret) {
 };
 
 /*
- * Calls stripe.handleCardPayment which creates a pop-up modal to
- * prompt the user to enter  extra authentication details without leaving your page
+ * Collect card details and pay for the order 
  */
 var pay = function(stripe, card) {
   var cardholderName = document.querySelector("#name").value;
@@ -96,7 +95,7 @@ var pay = function(stripe, card) {
 
   changeLoadingState(true);
 
-  // Initiate the payment. handleCardPayment will display a modal
+  // Collect card details
   stripe
     .createPaymentMethod("card", card, data)
     .then(function(result) {
